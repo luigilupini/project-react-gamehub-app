1a7006476a301354a29a05ac412df92bfbe26d45
Luigi Lupini <luigi.lupini@gmail.com>
2023-04-23 15:50:02 +0200

    - Moving all logic to Custom hook `useGame`

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,36 +1,8 @@
-import { useEffect, useState } from 'react';
-import apiClient from '../services/api-client';
 import ErrorMessage from './ErrorMessage';
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface GameResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from '../hooks/useGames';
 
 export default function GameGrid() {
-  const [games, setGames] = useState<Game[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
-
-  useEffect(() => {
-    setLoading(true);
-    apiClient
-      .get<GameResponse>('/games') // 👈🏻 We using the `/games` endpoint
-      .then((res) => {
-        setGames(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setLoading(false);
-      });
-  }, []);
-  console.log(games);
+  const { games, loading, error } = useGames();
   return (
     <div>
       {loading && <p>Loading...</p>}
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import apiClient from '../services/api-client';
+import { CanceledError } from 'axios';
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface GamesResponse {
+  count: number;
+  results: Game[];
+}
+
+export default function useGames() {
+  const [games, setGames] = useState<Game[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    setLoading(true);
+    // `AbortController` is a "built-in" browser API that allows us to `abort` a
+    // fetch request if the user navigates away from the page before the request
+    // has finished. This prevents the app from crashing. We can also use this
+    // to cancel a request if the user has already made a new request. This is a
+    // common pattern in React apps. We close the controller in the `return`
+    const controller = new AbortController();
+    // 👇🏻 We using the `/games` endpoint
+    apiClient
+      .get<GamesResponse>('/games', {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        // 👇🏻 We ignore the error if the request was canceled
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+    return () => controller.abort(); // 👈🏻 We close/cleanup the controller here
+  }, []);
+
+  return { games, loading, error };
+}

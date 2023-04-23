2ff397453c6f80e27b6fb67b2a53049711907825
Luigi Lupini <luigi.lupini@gmail.com>
2023-04-23 06:21:03 +0200

    - Create `<Navbar />` component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import { HStack, Image, Text } from '@chakra-ui/react';
+
+import logo from '../assets/logo/logo.webp';
+
+export default function Navbar() {
+  return (
+    <HStack>
+      <Image src={logo} boxSize="60px" />
+      <Text>Navbar</Text>
+    </HStack>
+  );
+}

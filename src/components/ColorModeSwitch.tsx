7433553188eb82f4a98ea09c963ed5e110b84957
Luigi Lupini <luigi.lupini@gmail.com>
2023-04-28 05:47:18 +0200

    - Create `SearchInput` component for Navbar

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -5,17 +5,21 @@ export default function ColorModeSwitch() {
   // and returns the color mode and function to toggle it
   const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <>
-      <HStack>
-        <Text fontSize="smaller" fontWeight="semibold">
-          {colorMode === 'dark' ? 'Dark Mode' : 'Light Mode'}
-        </Text>
-        <Switch
-          colorScheme="green"
-          isChecked={colorMode === 'dark'}
-          onChange={toggleColorMode}
-        />
-      </HStack>
-    </>
+    <HStack
+      paddingLeft={{
+        base: 0,
+        lg: 10,
+        xl: 20,
+      }}
+    >
+      <Text fontSize="smaller" fontWeight="semibold" whiteSpace="nowrap">
+        {colorMode === 'dark' ? 'Dark Mode' : 'Light Mode'}
+      </Text>
+      <Switch
+        colorScheme="green"
+        isChecked={colorMode === 'dark'}
+        onChange={toggleColorMode}
+      />
+    </HStack>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,20 @@
 import { HStack, Text } from '@chakra-ui/react';
 import ColorModeSwitch from './ColorModeSwitch';
+import SearchInput from './SearchInput';
 
 export default function Navbar() {
   return (
-    <HStack justifyContent="space-between" paddingY={6}>
+    <HStack paddingY={6} gap={2}>
       {/* <Image src={logo} boxSize="60px" /> */}
-      <Text fontWeight="extrabold" fontSize="18px">
+      <Text
+        fontWeight="extrabold"
+        fontSize="18px"
+        whiteSpace="nowrap"
+        letterSpacing={2}
+      >
         R A W G
       </Text>
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
